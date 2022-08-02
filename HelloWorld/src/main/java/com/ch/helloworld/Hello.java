b214f54dbdbac8437b579f6a06a028c4a7ca487b
unknown <curtisdhenley@gmail.com>
2022-08-01 20:05:16 -0400

    completed first program in netbeans

---
+++ HelloWorld/src/main/java/com/ch/helloworld/Hello.java
@@ -0,0 +1,15 @@
+
+package com.ch.helloworld;
+
+/**
+ *
+ * @author Curtis Henley
+ * email: henleydcurtis@gmail.com
+ * date: 08/01/2022
+ * purpose: My first Hello World program in NetBeans
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Curtis");
+    }
+}
